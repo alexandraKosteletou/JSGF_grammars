83006836e55591b8840fd671f997d78b28d23e0a
Alexandra Kosteletou <kosteletou.alex@gmail.com>
2022-01-16 00:27:58 +0200

    small addition

--- parse_grammar.py
+++ parse_grammar.py
@@ -1,27 +1,25 @@
 #parse jsgf grammar file
 from jsgf import parse_grammar_string
 
-# read grammar file
-#parse grammar file with parse_grammar_string function
+#read and parse grammar file with parse_grammar_string function
 with open ('grammar file path', 'r', encoding='utf-8' ) as f:
 
     grammar_file = f.read()
-
 grammar= parse_grammar_string(grammar_file)
-
+print(grammar)
 
 # function to find rules that match utterances
 def find_matching_rules(utt, grammar):
     try:
 
-        find_rule= grammar.find_matching_rules(line)[0]
+        find_rule= grammar.find_matching_rules(utt)[0]
         return True
 
     except:
         return False
 
-#use function to check if utterances from file match a rule 
-with open ('utterances file path', 'r', encoding='utf-8') as file:
+#use function to check if utterances from file match a rule
+with open ('tests file path', 'r', encoding='utf-8') as file:
 
     for line in file.readlines():
         assert find_matching_rules(line, grammar), line
